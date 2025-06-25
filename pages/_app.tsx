0d15c681246a47d5832db3e82b9553eb71a96951
Viacheslav Tymoschyk <velimyr@outlook.com>
2025-06-25 19:40:53 +0300

    add web analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'leaflet/dist/leaflet.css';
-
+import { Analytics } from '@vercel/analytics/react'; 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> 
+    </>
+  );
 }

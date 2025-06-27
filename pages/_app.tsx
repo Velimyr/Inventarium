7975d5d31a1bbb067cbd94243ded4b58d34c9814
Viacheslav Tymoschyk <velimyr@outlook.com>
2025-06-27 14:44:32 +0300

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'leaflet/dist/leaflet.css';
-import { Analytics } from '@vercel/analytics/react'; 
+import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* Можна також додати PNG-версію:
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" /> */}
+        <title>Inventarium</title> {/* Опціонально */}
+      </Head>
       <Component {...pageProps} />
-      <Analytics /> 
+      <Analytics />
     </>
   );
 }
